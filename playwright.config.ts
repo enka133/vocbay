b6f34e88c510e0f6e2ceba68748f072b60a0ccec
Enes Kaydak <enes.133@hotmail.com>
2026-05-21 17:23:30 +0400

    test: allow e2e base url override

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   testMatch: "*.e2e.ts",
   reporter: "line",
   use: {
-    baseURL: "http://127.0.0.1:5173",
+    baseURL: process.env.PLAYWRIGHT_BASE_URL ?? "http://127.0.0.1:5173",
     browserName: "chromium",
     hasTouch: true,
     isMobile: true,
